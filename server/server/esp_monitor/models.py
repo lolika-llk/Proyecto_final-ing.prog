9e364619c8af5df4636099b554be37d214f56f2c
lolika-llk <145618321+lolika-llk@users.noreply.github.com>
2025-06-06 15:41:18 -0600

    Revert "test"
    This reverts commit 73638d89f87ee1f81b3046da8356538b94b7d9e2.

--- server/server/esp_monitor/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class hist(models.Model):
-    temperatura=models.FloatField()
-    humedad=models.IntegerField()
-    fecha_hora=models.DateTimeField(auto_now_add=True)
--- server/server/esp_monitor/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns=[
-    path("",views.espmon,name='espmon'),
-    path("api",views.data,name="api")
-]
